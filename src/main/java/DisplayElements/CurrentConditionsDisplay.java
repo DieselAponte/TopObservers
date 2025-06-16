db47c96abd1aa837eea15dde4fd24ee733213aae
Diesel∈Aponte <143888820+DieselAponte@users.noreply.github.com>
2025-06-16 12:45:02 -0500

    Update CurrentConditionsDisplay.java

--- src/main/java/DisplayElements/CurrentConditionsDisplay.java
+++ src/main/java/DisplayElements/CurrentConditionsDisplay.java
@@ -3,6 +3,9 @@ package DisplayElements;
 import Subject.Subject;
 import Observer.Observer;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class CurrentConditionsDisplay implements Observer, DisplayElement{
     private float temperature;
     private float humidity;
@@ -10,9 +13,28 @@ public class CurrentConditionsDisplay implements Observer, DisplayElement{
     private float airQuality;
     private final Subject weatherData;
 
+    private final JPanel panel;
+    private final JLabel lbl_Temperature ;
+    private final JLabel lbl_Humidity ;
+    private final JLabel lbl_Pressure ;
+    private final JLabel lbl_AirQuality;
+
     public CurrentConditionsDisplay(Subject weatherData){
         this.weatherData = weatherData;
         weatherData.registerObserver(this);
+
+        panel = new JPanel(new GridLayout(4, 1));
+        panel.setBorder(BorderFactory.createTitledBorder("Condiciones Actuales"));
+
+        lbl_Temperature = new JLabel("Temperatura: ");
+        lbl_Humidity = new JLabel("Humedad: ");
+        lbl_Pressure = new JLabel("Presión: ");
+        lbl_AirQuality = new JLabel("Calidad del Aire: ");
+
+        panel.add(lbl_Temperature);
+        panel.add(lbl_Humidity);
+        panel.add(lbl_Pressure);
+        panel.add(lbl_AirQuality);
     }
 
     @Override
@@ -22,6 +44,11 @@ public class CurrentConditionsDisplay implements Observer, DisplayElement{
         this.pressure = pressure;
         this.airQuality = airQuality;
         display();
+
+        lbl_Temperature.setText("Temperatura: " + temperature + " °C");
+        lbl_Humidity.setText("Humedad: " + humidity + " %");
+        lbl_Pressure.setText("Presión: " + pressure + " Pa");
+        lbl_AirQuality.setText("Calidad del Aire: " + airQuality + " AQI");
     }
 
     @Override
@@ -32,6 +59,8 @@ public class CurrentConditionsDisplay implements Observer, DisplayElement{
                 ", pressure=" + pressure +
                 ", airQuality=" + airQuality);
     }
+
+
 
     public float getTemperature() {
         return temperature;
